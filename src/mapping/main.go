93cf5b6b228a5a1eba2591eef0a79cdbc13172f4
Jason Playne <jason@jasonplayne.com>
2016-10-06 19:37:09 +0800

    we now make a single web request for uncached tiles. latest leaflet.js. shiny new makefile

--- src/mapping/main.go
+++ src/mapping/main.go
@@ -4,8 +4,16 @@ import (
 	"fmt"
 )
 
+var (
+	rootDir string
+	version string
+	debug bool
+	build_type = ""
+)
+
 func main() {
-	fmt.Println("Start Mapping application")
+	debug = build_type=="debug"
+	fmt.Println("Start Mapping application, version:", version)
 	w := NewWebServer()
 	w.Run()
 }
--- src/mapping/store.go
+++ src/mapping/store.go
@@ -10,19 +10,27 @@ import (
 type store struct {
 	db   *bolt.DB
 	lock sync.Mutex
+	bucket []byte
+	path string
 }
 
-func NewStore(path string) (s *store, err error) {
+func NewStore(path, bucket string) (s *store, err error) {
 	s = &store{}
 	if path == "" {
 		path = "./tiles.db"
 	}
-	db, err := bolt.Open(path, 0600, nil)
+	if "" == bucket {
+		bucket = "tiles"
+	}
+	s.path = path
+	db, err := bolt.Open(s.path, 0600, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	s.bucket = []byte(bucket)
 	err = db.Update(func(tx *bolt.Tx) error {
-		tx.CreateBucketIfNotExists([]byte("tiles"))
+		tx.CreateBucketIfNotExists(s.bucket)
 		return nil
 	})
 	if err != nil {
@@ -40,7 +48,7 @@ func (s *store) Get(path string) (v interface{}, err error) {
 	var data []byte
 	//fmt.Println("store ", path)
 	err = s.db.View(func(tx *bolt.Tx) error {
-		data = tx.Bucket([]byte("tiles")).Get([]byte(path))
+		data = tx.Bucket(s.bucket).Get([]byte(path))
 		return nil
 	})
 	if err != nil {
@@ -55,8 +63,19 @@ func (s *store) Get(path string) (v interface{}, err error) {
 func (s *store) Put(path string, v interface{}) (err error) {
 	s.lock.Lock()
 	err = s.db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("tiles"))
+		b := tx.Bucket(s.bucket)
 		b.Put([]byte(path), v.([]byte))
+		return nil
+	})
+	s.lock.Unlock()
+	return
+}
+
+func (s *store) Remove(path string) (err error) {
+	s.lock.Lock()
+	err = s.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(s.bucket)
+		b.Delete([]byte(path))
 		return nil
 	})
 	s.lock.Unlock()
